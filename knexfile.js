8a958ce12b93f8fbc2b945e9c40b0f19d8298146
Kevin Tran <kevtran@hotmail.com>
2023-01-18 23:32:55 -0800

    undo knex adjustment

--- knexfile.js
+++ knexfile.js
@@ -15,26 +15,26 @@ module.exports = {
             charset: "utf8",
         },
     },
-    staging: {
-        client: "mysql",
-        connection: {
-            host: "hungry-database-backend.cmtseenrwa8c.us-west-2.rds.amazonaws.com",
-            port: "3306",
-            user: "root",
-            password: "rootroot",
-            database: "hungry-database",
-            charset: "utf8",
-        },
-    },
-    production: {
-        client: "mysql",
-        connection: {
-            host: "hungry-database-backend.cmtseenrwa8c.us-west-2.rds.amazonaws.com",
-            port: "3306",
-            user: "root",
-            password: "rootroot",
-            database: "hungry-database",
-            charset: "utf8",
-        },
-    },
+    // staging: {
+    //     client: "mysql",
+    //     connection: {
+    //         host: "hungry-database-backend.cmtseenrwa8c.us-west-2.rds.amazonaws.com",
+    //         port: "3306",
+    //         user: "root",
+    //         password: "rootroot",
+    //         database: "hungry-database",
+    //         charset: "utf8",
+    //     },
+    // },
+    // production: {
+    //     client: "mysql",
+    //     connection: {
+    //         host: "hungry-database-backend.cmtseenrwa8c.us-west-2.rds.amazonaws.com",
+    //         port: "3306",
+    //         user: "root",
+    //         password: "rootroot",
+    //         database: "hungry-database",
+    //         charset: "utf8",
+    //     },
+    // },
 };
