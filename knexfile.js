f9d05af3ac679152a79083da754630ca38c90c81
Kevin Tran <kevtran@hotmail.com>
2023-01-18 23:24:55 -0800

    Edit knex.js file

--- knexfile.js
+++ knexfile.js
@@ -15,36 +15,26 @@ module.exports = {
             charset: "utf8",
         },
     },
-
-    // staging: {
-    //   client: 'postgresql',
-    //   connection: {
-    //     database: 'my_db',
-    //     user:     'username',
-    //     password: 'password'
-    //   },
-    //   pool: {
-    //     min: 2,
-    //     max: 10
-    //   },
-    //   migrations: {
-    //     tableName: 'knex_migrations'
-    //   }
-    // },
-
-    // production: {
-    //   client: 'postgresql',
-    //   connection: {
-    //     database: 'my_db',
-    //     user:     'username',
-    //     password: 'password'
-    //   },
-    //   pool: {
-    //     min: 2,
-    //     max: 10
-    //   },
-    //   migrations: {
-    //     tableName: 'knex_migrations'
-    //   }
-    // }
+    staging: {
+        client: "mysql",
+        connection: {
+            host: "hungry-database-backend.cmtseenrwa8c.us-west-2.rds.amazonaws.com",
+            port: "3306",
+            user: "root",
+            password: "rootroot",
+            database: "hungry-database",
+            charset: "utf8",
+        },
+    },
+    production: {
+        client: "mysql",
+        connection: {
+            host: "hungry-database-backend.cmtseenrwa8c.us-west-2.rds.amazonaws.com",
+            port: "3306",
+            user: "root",
+            password: "rootroot",
+            database: "hungry-database",
+            charset: "utf8",
+        },
+    },
 };
